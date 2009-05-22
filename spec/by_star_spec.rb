05c06df8fd9fe92925a5d8fda269cbf9c2de21a3
Mislav Marohnić <mislav.marohnic@gmail.com>
2009-05-22 17:33:32 +0200

    cleanup specs

--- spec/by_star_spec.rb
+++ spec/by_star_spec.rb
@@ -1,153 +1,156 @@
 require 'spec_helper'
 
-# Used in a couple of tests, sometimes changes when we add new data.
-TOTAL_POSTS = 82
 describe Post do
-  
-  
-  it "should find all posts" do
-    Post.count.should eql(TOTAL_POSTS)
+
+  before do
+    @now = Time.now
   end
   
+  def find(*args)
+    method = description_args.first.sub(' ', '_')
+    Post.send(method, *args)
+  end
   
-  describe Post, "by year" do
+  describe "by year" do
     it "should be able to find all the posts in the current year" do
-      Post.by_year.size.should eql(TOTAL_POSTS - 1)
+      find.size.should eql(81)
     end
     
     it "should be able to find a single post from last year" do
-      Post.by_year(Time.now.year-1).size.should eql(1)
+      find(@now.year-1).size.should eql(1)
     end
   end
   
-  describe Post, "by month" do
-    
+  describe "by month" do
     it "should be able to find a post for the current month" do
-      Post.by_month.size.should eql(Time.now.month+3)
+      find.size.should eql(@now.month+3)
     end
   
     it "should be able to find a single post for January" do
-      Post.by_month("January").size.should eql(1)
+      find("January").size.should eql(1)
     end
   
     it "should be able to find two posts for the 2nd month" do
-      Post.by_month(2).size.should eql(2)
+      find(2).size.should eql(2)
     end
   
     it "should be able to find three posts for the 3rd month, using time instance" do
       # Hack... if we're running this test during march there's going to be more posts than usual.
       # This is due to the #today, #yesterday and #tomorrow methods.
       
-      Post.by_month(Time.local(Time.now.year, 3, 1)).size.should eql(3)
+      find(Time.local(@now.year, 3, 1)).size.should eql(3)
     end
   
     it "should be able to find a single post from January last year" do
-      Post.by_month("January", :year => Time.now.year - 1).size.should eql(1)
+      find("January", :year => @now.year - 1).size.should eql(1)
     end
     
     it "should fail when given incorrect months" do
-      lambda { Post.by_month(0) }.should raise_error(Frozenplague::ByStar::ParseError)
-      lambda { Post.by_month(13) }.should raise_error(Frozenplague::ByStar::ParseError)
-      lambda { Post.by_month("Ryan") }.should raise_error(Frozenplague::ByStar::ParseError)
-      lambda { Post.by_month([1,2,3])}.should raise_error(Frozenplague::ByStar::ParseError)
+      lambda { find(0) }.should raise_error(Frozenplague::ByStar::ParseError)
+      lambda { find(13) }.should raise_error(Frozenplague::ByStar::ParseError)
+      lambda { find("Ryan") }.should raise_error(Frozenplague::ByStar::ParseError)
+      lambda { find([1,2,3])}.should raise_error(Frozenplague::ByStar::ParseError)
     end
-    
   end
   
-  describe Post, "by fortnight" do
+  describe "by fortnight" do
     it "should be able to find posts in the 1st fortnight" do
-      Post.by_fortnight(1).size.should eql(1)
+      find(1).size.should eql(1)
     end
   end
   
-  describe Post, "by week" do
+  describe "by week" do
     it "should be able to find posts in the 1st week" do
-      Post.by_week(1).size.should eql(1)
+      find(1).size.should eql(1)
     end
     
     it "should be able to find posts in the 1st week of last year" do
-      Post.by_week(1, :year => Time.now.year-1).size.should eql(1)
+      find(1, :year => @now.year-1).size.should eql(1)
     end
   end
   
-  describe Post, "by day" do
+  describe "by day" do
     it "should be able to find a post for today" do
-      Post.by_day.size.should eql(1)
+      find.size.should eql(1)
     end
   end
 
-  describe Post, "today" do
+  describe "today" do
     it "should show the post for today" do
-      Post.today.map(&:text).should include("Today's post")
+      find.map(&:text).should include("Today's post")
     end
   end
   
-  describe Post, "yesterday" do
+  describe "yesterday" do
     it "should show the post for yesterday" do
-      Post.yesterday.map(&:text).should include("Yesterday's post")
+      find.map(&:text).should include("Yesterday's post")
     end
   end
   
-  describe Post, "tomorrow" do
+  describe "tomorrow" do
     it "should show the post for tomorrow" do
-      Post.tomorrow.map(&:text).should include("Tomorrow's post")
+      find.map(&:text).should include("Tomorrow's post")
     end
   end
   
-  describe Post, "past" do
+  describe "past" do
     it "should show the correct number of posts in the past" do
-      Post.past.size.should eql(Post.count(:conditions => ["created_at <= ?", Time.now]))
+      find(@now).size.should eql(Post.count(:conditions => ["created_at <= ?", @now]))
     end
   end
   
-  describe Post, "future" do
+  describe "future" do
     it "should show the correct number of posts in the future" do
-      Post.future.size.should eql(Post.count(:conditions => ["created_at >= ?", Time.now]))
+      find(@now).size.should eql(Post.count(:conditions => ["created_at >= ?", @now]))
     end
   end
   
-  describe Post, "as of" do
+  describe "as of" do
     it "should be able to find posts as of 2 weeks ago" do
-      year = Time.now.year
+      year = @now.year
       Time.stub!(:now).and_return("07-09-#{year}".to_time)
       Post.as_of_2_weeks_ago.size.should eql(9)
     end
     
     it "should not do anything if given an invalid date" do
-      lambda { Post.as_of_ryans_birthday }.should raise_error(Frozenplague::ByStar::ParseError, %(Chronic couldn't work out "Ryans birthday"; please be more precise))
+      lambda { Post.as_of_ryans_birthday }.should raise_error(
+        Frozenplague::ByStar::ParseError,
+        %(Chronic couldn't work out "Ryans birthday"; please be more precise)
+      )
     end
   end
   
-  describe Post, "up to" do
+  describe "up to" do
     it "should be able to find posts up to 2 weeks from now" do
-      year = Time.now.year
+      year = @now.year
       Time.stub!(:now).and_return("07-09-#{year}".to_time)
       Post.up_to_6_weeks_from_now.size.should eql(10)
     end
     
     it "should not do anything if given an invalid date" do
-      lambda { Post.up_to_ryans_birthday }.should raise_error(Frozenplague::ByStar::ParseError, %(Chronic couldn't work out "Ryans birthday"; please be more precise))
+      lambda { Post.up_to_ryans_birthday }.should raise_error(
+        Frozenplague::ByStar::ParseError,
+        %(Chronic couldn't work out "Ryans birthday"; please be more precise)
+      )
     end
-      
   end
   
-  # Because we override method_missing, we ensure that it still works as it should with this test.
-  describe Post, "find_by_text" do
+  describe "method_missing" do
     it "should still work" do
       Post.find_by_text("Today's post").should_not be_nil
     end
+  
+    it "should raise a proper NoMethodError" do
+      lambda { Post.idontexist }.should raise_error(NoMethodError, %r(^undefined method `idontexist'))
+    end
   end
     
-  describe Post, "nested find" do
+  describe "scoped find" do
     it "should be able to find a single post from January last year with the tag 'ruby'" do
-      month_scope = Post.by_month("January", :year => Time.now.year - 1)
+      month_scope = Post.by_month("January", :year => @now.year - 1)
       result = month_scope.all(:include => :tags, :conditions => ["tags.name = ?", 'ruby'])
       result.size.should eql(1)
     end
-  end
-  
-  it "should not completely hijack method_missing" do
-    lambda { Post.idontexist }.should raise_error(NoMethodError, %r(^undefined method `idontexist'))
   end
   
 end
