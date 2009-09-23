81d1360a9bf76c19f342b1622f1982b1794e261f
Ryan Bigg <radarlistener@gmail.com>
2009-09-24 09:44:59 +1000

    Move patchlevel check to inside 1.8.6 check.

--- spec/by_star_spec.rb
+++ spec/by_star_spec.rb
@@ -43,7 +43,7 @@ describe Post do
     it "should error when given an invalid year" do
       # This is broken on 1.8.6 (and previous versions), any patchlevel after & before 111
       major, minor, trivial = RUBY_VERSION.split(".").map(&:to_i)
-      if major == 1 && ((minor == 8 && trivial <= 6) || (minor <= 8)) && RUBY_PATCHLEVEL.to_i > 111
+      if major == 1 && ((minor == 8 && trivial <= 6 && RUBY_PATCHLEVEL.to_i > 111) || (minor <= 8))
         lambda { find(1901) }.should raise_error(ByStar::ParseError, "Invalid arguments detected, year may possibly be outside of valid range (1902-2039)")
         lambda { find(2039) }.should raise_error(ByStar::ParseError, "Invalid arguments detected, year may possibly be outside of valid range (1902-2039)")
       else
