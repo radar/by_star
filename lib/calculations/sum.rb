de7f3e6d7d2cc29b2f998020468cbc9eceac9633
Ryan Bigg <radarlistener@gmail.com>
2009-10-17 12:55:56 +1000

    Use Time.zone.now

--- lib/calculations/sum.rb
+++ lib/calculations/sum.rb
@@ -1,11 +1,11 @@
 module ByStar
   module Calculations
     module Sum
-      def sum_by_year(field, year=Time.now.year, options={})
+      def sum_by_year(field, year=Time.zone.now.year, options={})
         sum(field, :conditions => conditions_for_range(start_of_year(year), end_of_year(year), options))
       end
 
-      def sum_by_month(field, month=Time.now.month, options={})
+      def sum_by_month(field, month=Time.zone.now.month, options={})
         year, month = work_out_month(month, options)
         sum(field, :conditions => conditions_for_range(start_of_month(month, year), end_of_month(month, year), options))
       end
