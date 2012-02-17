82a3cb731980c0ccfe6f467e17e5eaa5b9e5933d
Ryan Bigg <radarlistener@gmail.com>
2012-02-17 13:41:05 -0500

    Add old by_star files as references

---
+++ lib/by_star.bak/calculations.rb
@@ -0,0 +1,23 @@
+module ByStar
+  module Calculations
+    include Count
+    include Sum
+
+    private
+      def work_out_month(time, year=Time.zone.now.year)
+        year ||= Time.zone.now.year
+        # Work out what actual month is.
+        month = if time.is_a?(Numeric) && (1..12).include?(time)
+          time
+        elsif valid_time_or_date?(time)
+          year = time.year
+          time.month
+        elsif time.is_a?(String) && Date::MONTHNAMES.include?(time)
+          Date::MONTHNAMES.index(time)
+        else
+          raise ParseError, "Value is not an integer (between 1 and 12), time object or string (make sure you typed the name right)."
+        end
+        [year, month]
+      end
+  end
+end
---
+++ lib/by_star.bak/version.rb
@@ -0,0 +1,3 @@
+module ByStar
+  VERSION = "1.0.1"
+end
