274405061cf8159650e4df90e6dfdaa2c951af22
Ryan Bigg <radarlistener@gmail.com>
2012-02-19 14:45:32 -0500

    Revert "Yesterday + Tomorrow posts need to be defined at the beginning_of_day so that they are findable within the scope of a weekend"
    This reverts commit 3ff5f558bed58cbeb67eff66fa94fb1d280e9e39.

--- spec/by_star/by_weekend_spec.rb
+++ spec/by_star/by_weekend_spec.rb
@@ -3,10 +3,12 @@ require 'spec_helper'
 describe 'by weekend' do
 
   it "should be able to find the posts on the weekend of the 1st of January" do
-    Post.by_weekend.count.should eql(10)
+    p Post.by_weekend.to_sql
+    posts_count = Post.by_weekend.count
+    posts_count.should eql(8)
   end
 
   it "should be able to use an alternative field" do
-    Event.by_weekend(:field => "start_time").count.should eql(3)
+    Event.by_weekend(:field => "start_time").count.should eql(2)
   end
 end
