ad23f4609c3e18902d2d073e5d20534ff8bc6bc4
Ryan Bigg <radarlistener@gmail.com>
2012-02-19 14:46:44 -0500

    Correct post + events count in by_weekend_spec

--- spec/by_star/by_weekend_spec.rb
+++ spec/by_star/by_weekend_spec.rb
@@ -3,12 +3,10 @@ require 'spec_helper'
 describe 'by weekend' do
 
   it "should be able to find the posts on the weekend of the 1st of January" do
-    p Post.by_weekend.to_sql
-    posts_count = Post.by_weekend.count
-    posts_count.should eql(8)
+    posts_count.should eql(10)
   end
 
   it "should be able to use an alternative field" do
-    Event.by_weekend(:field => "start_time").count.should eql(2)
+    Event.by_weekend(:field => "start_time").count.should eql(3)
   end
 end
