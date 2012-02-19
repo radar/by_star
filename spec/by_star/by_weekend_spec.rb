761b4cf35bb49f3faf83ba8daeba8ee902d0bfde
Ryan Bigg <radarlistener@gmail.com>
2012-02-19 13:22:26 -0500

    Show SQL for by_weekend query on Travis (still debugging build #9)

--- spec/by_star/by_weekend_spec.rb
+++ spec/by_star/by_weekend_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'by weekend' do
 
   it "should be able to find the posts on the weekend of the 1st of January" do
-    p Post.by_weekend.map(&:text)
+    p Post.by_weekend.to_sql
     posts_count = Post.by_weekend.count
     posts_count.should eql(8)
   end
