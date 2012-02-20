fc345de292e20524fb8cb1aa8b4c11744997f80e
Ryan Bigg <radarlistener@gmail.com>
2012-02-19 22:03:29 -0500

    Remove tags, duplicate posts, invoices and date entries from models
    We are no longer testing these, because if we were, we would be testing
    AR functionality, and that's boring

--- spec/fixtures/models.rb
+++ spec/fixtures/models.rb
@@ -7,24 +7,9 @@ class Post < ActiveRecord::Base
   end
 end
 
-class Tag < ActiveRecord::Base
-  has_and_belongs_to_many :posts
-end
-
 class Event < ActiveRecord::Base
   by_star_field :start_time
   scope :secret, :conditions => { :public => false }
-end
-
-class Invoice < ActiveRecord::Base
-  has_many :day_entries
-  def self.factory(value, created_at = nil)
-    create!(:value => value, :created_at => created_at, :number => value)
-  end
-end
-
-class DayEntry < ActiveRecord::Base
-
 end
 
 ## seed data:
@@ -34,19 +19,6 @@ year = Time.zone.now.year
 1.upto(12) do |month|
   Post.factory "post #{month}", Time.zone.now.beginning_of_year + (month - 1).months
 end
-
-1.upto(12) do |month|
-  Invoice.factory 10000, Time.zone.now.beginning_of_year + (month - 1).months
-end
-
-# Inovice for 2nd January for sum_by_day
-Invoice.factory 5500, Time.zone.now.beginning_of_year + 1.day
-
-# Invoice from last year
-Invoice.factory 10000, Time.local(Time.zone.now.year-1, 1, 1)
-
-# Invoice without a number for count_by_year test
-Invoice.create!(:value => 10000, :number => nil)
 
 Post.factory "Today's post", Time.zone.now
 Post.factory "Yesterday's post", Time.zone.now - 1.day
@@ -61,31 +33,11 @@ weekend_time += 1.day while weekend_time.wday != 6
 
 # For by_weekend scoped test
 post = Post.factory "Weekend", weekend_time
-post.tags.create(:name => "weekend")
 
-# For by_day scoped test
-post = Post.factory "Today", Time.zone.now
-post.tags.create(:name => "today")
+# Offset by two seconds to stop it clashing with "Today's post" in next test
+post = Post.factory "The 'Current' Fortnight", Time.zone.now + 2.seconds
 
-# For yesterday scoped test
-post = Post.factory "Yesterday", Time.zone.now.yesterday
-post.tags.create(:name => "yesterday")
-
-# For tomorrow scoped test
-post = Post.factory "Tomorrow's Another Day", Time.zone.now.tomorrow
-post.tags.create(:name => "tomorrow")
-
-post = Post.factory "Last year", Time.zone.now.beginning_of_year - 1.year
-post.tags.create(:name => "ruby")
-
-post = Post.factory "End of last year", Time.zone.now.end_of_year - 1.year
-post.tags.create(:name => "final")
-
-post = Post.factory "The 'Current' Fortnight", Time.zone.now
-post.tags.create(:name => "fortnight")
-
-post = Post.factory "The 'Current' Week", Time.zone.now
-post.tags.create(:name => "week")
+post = Post.factory "The 'Current' Week", Time.zone.now + 2.seconds
 
 
 Event.create(:name => "Ryan's birthday!", :start_time  => "04-12-#{Time.zone.now.year}".to_time)
