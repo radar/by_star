7eeca50f97ebaf488c547151d560938921dfe55a
Ryan Bigg <radarlistener@gmail.com>
2012-02-19 21:55:46 -0500

    Use reorder in previous+next to ensure that no other order clause interferes

--- lib/by_star/instance_methods.rb
+++ lib/by_star/instance_methods.rb
@@ -2,12 +2,12 @@ module ByStar
   module InstanceMethods
     def previous(options={})
       field = options[:field] || self.class.by_star_field
-      self.class.where("#{field} < ?", self.send(field)).order("#{field} DESC").first
+      self.class.where("#{field} < ?", self.send(field)).reorder("#{field} DESC").first
     end
 
     def next(options={})
       field = options[:field] || self.class.by_star_field
-      self.class.where("#{field} > ?", self.send(field)).order("#{field} ASC").first
+      self.class.where("#{field} > ?", self.send(field)).reorder("#{field} ASC").first
     end
   end
 end
