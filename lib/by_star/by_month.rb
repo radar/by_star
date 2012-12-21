6b3990248a2122f032c08049295f731110e1bfdb
Krishnaprasad T Nair <krishnaprasad@mobme.in>
2012-12-21 17:37:56 +0530

    works for most formats for month

--- lib/by_star/by_month.rb
+++ lib/by_star/by_month.rb
@@ -16,20 +16,18 @@ module ByStar
     end
 
     def by_month_String_or_Fixnum(month, options={})
-      if valid_month?(month)
+      begin
         year = options[:year] || Time.zone.now.year
-        by_month_Time("#{year}-#{month}-01".to_time, options)
-      else
+        date = Date.parse("#{year}-#{month}-01").to_time
+        by_month_Time(date, options)
+
+      rescue
         raise ParseError, "Month must be a number between 1 and 12 or the full month name (e.g. 'January', 'Feburary', etc.)"
       end
     end
+
     alias_method :by_month_String, :by_month_String_or_Fixnum
     alias_method :by_month_Fixnum, :by_month_String_or_Fixnum
-
-
-    def valid_month?(month)
-      (1..12).include?(month) || Date::MONTHNAMES.include?(month)
-    end
 
   end
 end
