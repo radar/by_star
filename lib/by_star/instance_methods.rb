c5f2789c07681d65ca7d48e6bd810ab24c010f3b
Phil <pnomolos@gmail.com>
2013-07-07 21:13:08 -0700

    Allowing to pass scope to previous/next methods

--- lib/by_star/instance_methods.rb
+++ lib/by_star/instance_methods.rb
@@ -2,12 +2,12 @@ module ByStar
   module InstanceMethods
     def previous(options={})
       field = options[:field] || self.class.by_star_field
-      self.class.where("#{field} < ?", self.send(field)).reorder("#{field} DESC").first
+      (options[:scope] || self.class).where("#{field} < ?", self.send(field)).reorder("#{field} DESC").first
     end
 
-    def next(options={})
+    def next(options={}, scope = nil)
       field = options[:field] || self.class.by_star_field
-      self.class.where("#{field} > ?", self.send(field)).reorder("#{field} ASC").first
+      (options[:scope] || self.class).where("#{field} > ?", self.send(field)).reorder("#{field} ASC").first
     end
   end
 end
