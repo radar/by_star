bcd45e7c106bd92681fe391be856af1026f73e01
jagdeepsingh <jagdeepsingh.125k@gmail.com>
2019-06-06 16:07:47 +0530

    Fix default scope

--- spec/fixtures/active_record/models.rb
+++ spec/fixtures/active_record/models.rb
@@ -8,5 +8,5 @@ end
 class Event < ActiveRecord::Base
   by_star_field :start_time, :end_time, offset: 3.hours
 
-  default_scope ->{ order(day_of_month: :asc) }
+  default_scope ->{ order('day_of_month ASC') }
 end
